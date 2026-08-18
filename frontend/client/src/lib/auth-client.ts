dbc316ca496550ed5d281ababa8d1e00a3b6b32d
Thepipsqueak <98383696+Thepipsqueak62@users.noreply.github.com>
2026-08-18 19:37:58 -0400

    Updated auth-client.ts
    Added AppUser Inferface

--- frontend/client/src/lib/auth-client.ts
+++ frontend/client/src/lib/auth-client.ts
@@ -1,13 +1,24 @@
-import {createAuthClient} from "better-auth/client";
-import {adminClient} from "better-auth/client/plugins";
-import {ac, admin, doctor, nurse} from "./permissions";
+import { createAuthClient } from "better-auth/react";
+import { adminClient } from "better-auth/client/plugins";
+import { ac, admin, doctor, nurse } from "./permissions";
 
 export const authClient = createAuthClient({
     baseURL: 'http://localhost:4000',
     plugins: [
-        adminClient({ ac, roles: { admin, doctor, nurse } }
-        ),
+        adminClient({ ac, roles: { admin, doctor, nurse } }),
     ],
 })
 
 
+export interface AppUser {
+    id: string;
+    email: string;
+    name: string;
+    role: "admin" | "user" | "doctor" | "nurse";
+    phoneNumber?: string | null;
+    dateOfBirth?: string | null;
+    emailVerified: boolean;
+    image?: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+}
