9d1906d685495b9d1dd8970a03dfac98e502723b
Thepipsqueak <98383696+Thepipsqueak62@users.noreply.github.com>
2026-08-18 04:27:58 -0400

    Fleshed out the entire admin.panel

--- frontend/client/src/pages/adminPage.tsx
+++ frontend/client/src/pages/adminPage.tsx
@@ -1,8 +1,44 @@
+import {useUserStore} from "@/store/Userstore.ts";
+import {authClient} from "@/lib/auth-client.ts";
+import {Button} from "@/components/ui/button.tsx";
+
 const AdminPage = () => {
+    const user = useUserStore((s) => s.user);
+    const clearUser = useUserStore((s) => s.clearUser);
+    const setStatus = useUserStore((s) => s.setStatus);
+    async function handleSignOut(){
+        const {error} = await authClient.signOut();
+
+        clearUser()
+        setStatus(error? `Error: ${error.message}`:"Signed Out");
+
+    }
+
     return (
         <div>
-            ADMIN PAGE
+          <div>
+              {user && (
+                  <div>
+                      <p>{user.name} Welcome to the admin panel</p>
+                      <p>Role: {user.role}</p>
+                      <p>Email: {user.email}</p>
+                      <p>PhoneNumber: {user.phoneNumber}</p>
+                      <p>Date Of Birth: {user.dateOfBirth}</p>
+                      <p>Created At Date: {new Date(user.createdAt).toLocaleString()}</p>
+                      <p>Last Updated: {new Date(user.updatedAt).toLocaleString()}</p>
+                      <p>isEmailVerified: {user.emailVerified ?"Yes":"No"}</p>
+                      <p>User id: {user.id}</p>
+
+
+                  </div>
+              )}
+          </div>
+            <Button onClick={handleSignOut}>
+                SIGN OUT
+            </Button>
+
         </div>
+
     );
 };
 
